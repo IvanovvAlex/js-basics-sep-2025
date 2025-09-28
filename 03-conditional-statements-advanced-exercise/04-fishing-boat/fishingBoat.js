555e4f04fb5a37da1d756d1b809374fd7428893e
Alex Ivanov <contact@alex-ivanov.com>
2025-09-28 12:58:44 +0300

    Add JavaScript exercises for cinema, summer outfit, new house, fishing boat, journey, operations between numbers, hotel room, on time for exam, and ski vacation with detailed implementations

--- 03-conditional-statements-advanced-exercise/04-fishing-boat/fishingBoat.js
+++ 03-conditional-statements-advanced-exercise/04-fishing-boat/fishingBoat.js
@@ -1,1 +1,38 @@
-function fishingBoat() {}
+function fishingBoat(budget, season, fishermen) {
+  let boatPrice = 0;
+
+  switch (season) {
+    case "Spring":
+      boatPrice = 3000;
+      break;
+    case "Summer":
+    case "Autumn":
+      boatPrice = 4200;
+      break;
+    case "Winter":
+      boatPrice = 2600;
+      break;
+  }
+
+  if (fishermen <= 6) {
+    boatPrice *= 0.9;
+  } else if (fishermen >= 7 && fishermen <= 11) {
+    boatPrice *= 0.85;
+  } else if (fishermen >= 12) {
+    boatPrice *= 0.75;
+  }
+
+  if (fishermen % 2 === 0 && season !== "Autumn") {
+    boatPrice *= 0.95;
+  }
+
+  if (budget >= boatPrice) {
+    let moneyLeft = budget - boatPrice;
+
+    console.log(`Yes! You have ${moneyLeft.toFixed(2)} leva left.`);
+  } else {
+    let moneyNeeded = (boatPrice - budget).toFixed(2);
+
+    console.log(`Not enough money! You need ${moneyNeeded} leva.`);
+  }
+}
--- 03-conditional-statements-advanced-exercise/09-ski-vacation/skiVacation.js
+++ 03-conditional-statements-advanced-exercise/09-ski-vacation/skiVacation.js
@@ -1,1 +1,41 @@
-function skiVacation() {}
+function skiVacation(days, roomType, rating) {
+  const nights = days - 1;
+  let price = 0;
+
+  switch (roomType) {
+    case "room for one person":
+      price = nights * 18.0;
+      break;
+    case "apartment":
+      price = nights * 25.0;
+      if (nights < 10) {
+        price *= 0.7;
+      } else if (nights >= 10 && nights <= 15) {
+        price *= 0.65;
+      } else {
+        price *= 0.5;
+      }
+      break;
+    case "president apartment":
+      price = nights * 35.0;
+      if (nights < 10) {
+        price *= 0.9;
+      } else if (nights >= 10 && nights <= 15) {
+        price *= 0.85;
+      } else {
+        price *= 0.8;
+      }
+      break;
+  }
+
+  switch (rating) {
+    case "positive":
+      price *= 1.25;
+      break;
+    case "negative":
+      price *= 0.9;
+      break;
+  }
+
+  console.log(price.toFixed(2));
+}
